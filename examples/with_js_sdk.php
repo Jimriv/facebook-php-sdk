0e96597b47db9e78471d74a23652880a228e6510
Jimriv <bitungpost24@gmail.com>
2013-08-09 04:18:45 -0700

    Revert "Update with_js_sdk.php"
    This reverts commit dcb724e83053c1f2393d44926cc93c6094b2104f.

--- examples/with_js_sdk.php
+++ examples/with_js_sdk.php
@@ -3,8 +3,8 @@
 require '../src/facebook.php';
 
 $facebook = new Facebook(array(
-  'appId'  => '346556795377824',
-  'secret' => 'e285e5775e43715c8a642ab9ebb73bee',
+  'appId'  => '344617158898614',
+  'secret' => '6dc8ac871858b34798bc2488200e503d',
 ));
 
 // See if there is a user from a cookie
